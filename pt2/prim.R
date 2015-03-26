0e9a4d9d496278927ed9286a34e11e45cbf17281
tcosta <tcosta@uolinc.com>
2015-03-26 14:46:50 -0300

    updating code

--- pt2/prim.R
+++ pt2/prim.R
@@ -2,21 +2,39 @@
 ##Spanning tree: no cycles, all connected nodes are spanned
 ##Computes minimum cost satisfy above criteria
 
-input <- read.csv('edges.txt', header = FALSE, sep = " ")
-num_nodes<-input[1, 'V1']
-num_edges<-input[1, 'V2']
-
-data <- input[2:nrow(input),]
-
-random_row<-as.numeric(data[sample(1:nrow(data),1),])
-all_rows <- as.numeric(data[,1])
-X <- c(random_row[1])
-V <- c(unique(all_rows))
-
-dict<-split(data,data$V1)
-
-while(all(X,V)==FALSE){
+prim <- function(){
   
+  input <- read.csv('edges-text.txt', header = FALSE, sep = " ")
+  num_nodes<-input[1, 'V1']
+  num_edges<-input[1, 'V2']
   
+  data <- input[2:nrow(input),]
   
+  random_row<-as.numeric(data[sample(1:nrow(data),1),])
+  all_rows <- as.numeric(data[,1])
+  X <- c(random_row[1])
+  V <- c(unique(all_rows))
+  A <- c()
+  
+  cost <- 0
+  
+  i <- 0
+  
+  while(length(intersect(X,V))!=length(V)){
+    
+    k <- X[1]
+    e <- subset(data, V1==k)
+    
+    if(!e[2] %in% A){
+      A <- c(e[2],A)
+      X <- c(e[1],X)
+      
+      cost <- cost + e[3]
+    }
+    
+    i<- i+1
+    
+  }
+  
+  print(paste("MST Cost" ,cost, sep = " "))
 }
