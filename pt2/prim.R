a358aaa57cfe1ff04d9d92a5a93a8b1742e3aacb
Vinicius <vinicius.remigio@gmail.com>
2015-03-30 22:42:54 -0300

    final version

--- pt2/prim.R
+++ pt2/prim.R
@@ -1,38 +1,41 @@
-##Minimum spanning trees
-##Spanning tree: no cycles, all connected nodes are spanned
-##Computes minimum cost satisfy above criteria
+## Prim's Algorithm - Minimum spanning trees
+## Spanning tree: 1. no cycles, 2. all connected nodes are spanned
+## Compute minimum cost which satisfies above criteria
+
+## Time complexity: O(|E|log|V|), if adjacency list is a heap ( which is not the case :) )
+
+## Dijkstra shortest path only computes minimal cost between s->t. Special case of Prim's
 
 prim <- function(){
   
-  input <- read.csv('edges-text.txt', header = FALSE, sep = " ")
+  #Read file
+  input <- read.csv('edges.txt', header = FALSE, sep = " ")
   num_nodes<-input[1, 'V1']
   num_edges<-input[1, 'V2']
   
-  data <- input[2:nrow(input),]
+  #Generate undirected graph
+  n_data   <- input[2:nrow(input),]
+  i_data <- data.frame(n_data$V2, n_data$V1, n_data$V3)
+  colnames(i_data) <- c('V1','V2','V3')
+  data <- rbind(n_data,i_data)
   
+  #Init step
   random_row<-as.numeric(data[sample(1:nrow(data),1),])
   all_rows <- as.numeric(data[,1])
   X <- c(random_row[1])
   V <- c(unique(all_rows))
-  A <- c()
-  
+  E <- c() 
   cost <- 0
   
-  i <- 0
-  
+  #main loop
   while(length(intersect(X,V))!=length(V)){
     
-    k <- X[1]
-    e <- subset(data, V1==k)
-    
-    if(!e[2] %in% A){
-      A <- c(e[2],A)
-      X <- c(e[1],X)
-      
-      cost <- cost + e[3]
-    }
-    
-    i<- i+1
+    edges <- subset(data, !row.names(data) %in% E & V1 %in% X & !V2 %in% X)
+    min_e <- edges[order(edges$V3),][1,]
+  
+    X <- c(X,as.numeric(min_e[2]))
+    E <- c(E,as.numeric(row.names(min_e)))
+    cost <- cost + as.numeric(min_e[3])
     
   }
   
