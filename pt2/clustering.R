601c4ebc5ca89e359636a65b4dca113153d5e110
tcosta <tcosta@uolinc.com>
2015-04-03 21:38:18 -0300

    question1

--- pt2/clustering.R
+++ pt2/clustering.R
@@ -1,58 +1,66 @@
-union <- function(c1,c2){
-  c12 <- c(c1,c2)
-  c12
-}
-
-find <- function(x,clusters){
-  cluster <- NULL
-  for(c in clusters){
-    if(x %in% c){
-      cluster <- c
+find <- function(n,clusters){
+  name <- NULL
+  for (i in 1:length(clusters)){
+    if(n %in% clusters[[i]]){
+      name <- names(clusters[i])
+      break
     }
   }
-  cluster
+  name
 }
 
-## Kruskal's union-find algorithm to compute max spacing k-clustering
+## Kruskal's union-find based algorithm to compute max spacing k-clustering
 ## Complexity O (|V| log |E|) same as heap-based prim's algorithm
-max_spacing <- function(num_clusters){
+max_spacing <- function(num_clusters, filename){
   
-  input <- read.csv('cluster-test.txt', header = FALSE, sep = " ")
+  input <- read.csv(filename, header = FALSE, sep = " ")
   num_nodes<-input[1, 'V1']
   
   ##Generate undirected graph
   n_data <- input[2:nrow(input),]
   k <- nrow(n_data)-1
   
-  #i_data <- data.frame(n_data$V2, n_data$V1, n_data$V3)
-  #colnames(i_data) <- c('V1','V2','V3')
-  #data <- rbind(n_data,i_data)
-  
-  #sort by increasing order
+  #sort by increasing order, i. e. nearest vertices
   sorted_data <- n_data[order(n_data$V3),]
   
   #init clusters
   clusters <- list()
-  clusters[1:k] <- 1:k
+  clusters[1:num_nodes] <- 1:num_nodes
+  names(clusters) <- 1:num_nodes
   
-  i <- 0
-  n <- 0
+  i <- 1
+  n <- length(clusters)
   
+  #while number of desired clusters not reached, then group nearest vertices
   while(n != num_clusters){
   
-    edge <- sorted_data[i+1,]
-    clusters[[edge$V1]] <- c(clusters[[edge$V1]], clusters[[edge$V2]])
-    clusters[edge$V2] <- list(NULL)
+    edge <- sorted_data[i,]
+    
+    #find operation
+    c1 <- find(edge$V1,clusters)
+    c2 <- find(edge$V2,clusters)
+  
+    i <- i + 1
+    if(c1 == c2){
+      next
+    }
+    
+    #union operation
+    clusters[[c1]] <- c(clusters[[c1]], clusters[[c2]])
+    clusters[c2] <- list(NULL)
 
     n <- length(Filter(Negate(is.null),clusters))
-    i <- i + 1
   }
   
+  #delete empty clusters
   filtered <- Filter(Negate(is.null),clusters)
   
-  for(list in filtered){
-    for(element in list){
-      print(element)
-    }
+  #retrieve value that minimizes max spacing
+  min <- c()
+  for (i in 1:length(filtered)){
+    result <- subset(n_data, V1 %in% filtered[[i]] & V2 %in% unlist(filtered[-i]), select = V3)
+    min <- c(min,as.vector(result$V3))
   }
+  
+  min[order(min)][1]
 }
